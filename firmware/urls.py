33867cb841ffe7ea606585a9e045945ff49d6942
Andrii <zhuravlovandrii04@gmail.com>
2024-11-01 16:46:58 +0200

    logs

--- firmware/urls.py
+++ firmware/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),  # For login/logout
     path('sync_vtiger/', views.sync_vtiger, name='sync_vtiger'),
     path('api/create_asset/', views.create_asset, name='create_asset'),
-    path('api/logs', views.logs, name='logs'),
+    path('api/logs/', views.logs, name='logs'),
 
 ]
